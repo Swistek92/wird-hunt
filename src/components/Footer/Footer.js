5a001a141b3fb3db6692bff8ec4b613f4a7fb335
Swistek92 <swistekxD@gmail.com>
2021-09-06 21:57:15 +0200

    1

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <hr style={{ width: "90%", marginTop: 20 }} />
       <span className="name">
         Made by{" "}
-        <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
+        <a href="https://www.facebook.com/Swistekk" target="__blank">
           Piotreck
         </a>
       </span>
